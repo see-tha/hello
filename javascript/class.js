1e091fc7304af3684a112f66301aa1c9376ab751
Seethalakshmi Murali <112931728+see-tha@users.noreply.github.com>
2022-10-03 17:47:39 +0530

    Add files via upload

---
+++ javascript/class.js
@@ -0,0 +1,19 @@
+class Person{
+
+    constructor(name)
+
+    {
+
+        this.name=name;
+
+    }
+
+}
+
+const p1 = new  Person('Sree');
+
+const p2 = new  Person('seetha');
+
+console.log(p1.name);
+
+console.log(p2.name);
---
+++ javascript/class2.js
@@ -0,0 +1,20 @@
+class Person{
+
+    constructor(name,id)
+
+    {
+
+        this.name=name;
+        this.id=id;
+
+    }
+
+}
+
+const p1 = new  Person('chippy',233757);
+
+const p2 = new  Person('chinnu',244567);
+
+console.log(p1.name, p1.id);
+
+console.log(p2.name,p2.id);
